b9a92e7ba067846dc1075338d822a4a0f2211675
anlapro123 <85329350+anlapro123@users.noreply.github.com>
2021-06-04 11:44:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     introTitle: 'Trân à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ mập,
+    Mà sao em cứ mập`,
     btnIntro: 'cak',
     title: 'Trân có mập không 😙',
-    desc: 'Trân có mập không ',
+    desc: 'Trân có mập không  ',
     btnYes: 'Mập :D',
     btnNo: 'Không mập',
     question:'Trên thế giới hơn 7 tỉ người mà sao em mập thế <3',
     btnReply: 'cak',
-    reply: 'mập thì mập mà không mập thì mập <33333333',
+    reply: 'Mập thì mập mà không mập thì mập',
     mess: 't biết mà.',
     messDesc: 'Mập thì mập rồi tối nay 7h tao qua đón đi ăn nhé.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/messages/t/100005005146664'
+    messLink: https://www.facebook.com/thanh.luan.83
 }
