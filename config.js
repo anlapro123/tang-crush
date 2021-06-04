551feb905c403d9a717c180981f9674a4c04421a
anlapro123 <85329350+anlapro123@users.noreply.github.com>
2021-06-04 11:48:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,19 @@
 const CONFIG = {
-    introTitle: 'Trân à!',
+    introTitle: 'Babe à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ mập`,
-    btnIntro: 'cak',
-    title: 'Trân có mập không 😙',
-    desc: 'Trân có mập không  ',
-    btnYes: 'Mập :D',
-    btnNo: 'Không mập',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em mập thế <3',
-    btnReply: 'cak',
-    reply: 'Mập thì mập mà không mập thì mập',
-    mess: 't biết mà.',
-    messDesc: 'Mập thì mập rồi tối nay 7h tao qua đón đi ăn nhé.',
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: https://www.facebook.com/thanh.luan.83
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
